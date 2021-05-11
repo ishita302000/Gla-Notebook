fa3ecb714a231eb572e9e099230ef779517d962e
ishita302000 <ishitagoel3020@gmail.com>
2021-05-11 09:30:10 +0530

    mini project

--- app/src/main/java/com/example/glanotebook/course.java
+++ app/src/main/java/com/example/glanotebook/course.java
@@ -2,13 +2,31 @@ package com.example.glanotebook;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class course extends AppCompatActivity {
+    private Button btn;
+    private TextView tt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course);
+
+        tt = findViewById(R.id.tv1);
+        btn = findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(course.this , branch.class);
+                startActivity(i);
+                finish();
+            }
+        });
     }
 }
